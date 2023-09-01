70aae51d26c4e53635490df2f86323eb2b606eac
Samuel <samuelcoryn@gmail.com>
2023-09-01 18:59:55 +0200

    Customiztaion

--- quartz.config.ts
+++ quartz.config.ts
@@ -20,8 +20,8 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
+          light: "#FEF7EA",
+          lightgray: "#FEEDCD",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",
